0056291ae5562b646f6d594923337cf8fffe0cfd
CookieMonsterSports <anibalureno.alcom@gmail.com>
2026-07-29 11:29:25 -0500

    Update app.js

--- css/js/app.js
+++ css/js/app.js
@@ -1,1 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
 
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>CookieMonster Sports</title>
+
+<link rel="stylesheet" href="css/estilos.css">
+
+</head>
+
+<body>
+
+<header>
+
+<h1>CookieMonster Sports</h1>
+
+<input
+type="text"
+placeholder="Buscar camiseta..."
+id="buscar">
+
+</header>
+
+<main>
+
+<div id="catalogo">
+
+</div>
+
+</main>
+
+<script src="js/app.js"></script>
+
+</body>
+
+</html>
