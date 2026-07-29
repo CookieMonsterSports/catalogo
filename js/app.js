bd32ba3b89355ce09a4696466284a9b5602c12a0
CookieMonsterSports <anibalureno.alcom@gmail.com>
2026-07-29 11:50:51 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+const catalogo=document.getElementById("catalogo");
 
+catalogo.innerHTML=`
+
+<div style="
+background:white;
+border-radius:12px;
+overflow:hidden;
+box-shadow:0 2px 10px rgba(0,0,0,.15);
+">
+
+<img
+src="https://picsum.photos/400/500"
+style="width:100%;display:block;">
+
+<div style="padding:18px;">
+
+<h3>Real Madrid</h3>
+
+<p>25/26 Home Player</p>
+
+</div>
+
+</div>
+
+`;
